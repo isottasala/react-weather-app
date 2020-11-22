c6da2ed1bbc50e671a950b8cde86c6720b68add7
isottasala <69543273+isottasala@users.noreply.github.com>
2020-11-22 14:31:47 +0100

    Added GitHub repository url

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-          Learn React
+          Learn React 
+          <footer>Open Source-Code by <a href="https://github.com/isottasala/react-weather-app" target="_blank" rel="noreferrer">Isotta Sala</a></footer>
       </header>
     </div>
   );
